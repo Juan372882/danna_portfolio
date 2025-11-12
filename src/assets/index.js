57d024fe38030e3a7841f1d710aa147539a80afe
Juan <jmmartinez4817@misena.edu.co>
2025-11-11 20:58:48 -0500

    Experience Finished

--- src/assets/index.js
+++ src/assets/index.js
@@ -21,10 +21,9 @@ import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
 import threejs from "./tech/threejs.svg";
 
-import meta from "./company/meta.png";
-import shopify from "./company/shopify.png";
-import starbucks from "./company/starbucks.png";
-import tesla from "./company/tesla.png";
+import belleza from "./company/belleza.png";
+import gestcol from "./company/gestcol.png";
+import teleperformance from "./company/teleperformance.png";
 
 import carrent from "./carrent.png";
 import jobit from "./jobit.png";
@@ -52,10 +51,9 @@ export {
   tailwind,
   typescript,
   threejs,
-  meta,
-  shopify,
-  starbucks,
-  tesla,
+  belleza,
+  gestcol,
+  teleperformance,
   carrent,
   jobit,
   tripguide,
